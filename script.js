18637f4bd8a3fc4c7ab8ffce211b26f9dbc515a8
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-06-17 16:42:12 +1000

    created and styled todo-board

---
+++ script.js
@@ -0,0 +1,5 @@
+const myList = document.querySelector('#todo-list');
+const myInput = document.querySelector('#todo-input');
+const myButton = document.querySelector('#add-button');
+const deleteButton = document.querySelector('#delete-button');
+const completeButton = document.querySelector('#complete-button');
